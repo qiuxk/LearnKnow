3257beff0fc3d19e4de21589aacb69af93fa2d52
qiuxk <qxk1101@163.com>
2018-02-05 19:12:50 +0800

    LinkedList源码分析

---
+++ src/com/qiuxk/ListSourceCode/LinkedListDemo.java
@@ -0,0 +1,20 @@
+package com.qiuxk.ListSourceCode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @description:
+ * @auth: qiuxk
+ * @createDate：2018/2/5 18:10
+ **/
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+        //LinkedList 是List接口的链式列表实现
+        //允许所以元素为null
+
+        List<String> list=new LinkedList<String>();
+    }
+
+}
